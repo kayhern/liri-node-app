0df77a0a614281ccf556afe2c85afac8ba41f0c2
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-21 20:58:09 -0500

    moment install

--- liri.js
+++ liri.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
+var moment = require('moment');
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+const axios = require('axios');
 
 var spotify = new Spotify(keys.spotify);
 
 //need to install npm packages
 //Spotify: https://www.npmjs.com/package/node-spotify-api
 //Axios: https://www.npmjs.com/package/axios
-//Moment:
+//Moment: https://momentjs.com/docs/
 //DotEnv
 //step 9
