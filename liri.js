037aeaa544338e7e8ad0951524ef916698f09356
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-21 20:17:45 -0500

    spotify package akage

--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
 //need to install npm packages
-
+//Spotify: https://www.npmjs.com/package/node-spotify-api
+//Axios: https://www.npmjs.com/package/axios
 //step 9
