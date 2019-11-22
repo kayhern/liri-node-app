be88b495ab26b5e4d2b566d99cd63812c42653d1
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-21 21:02:32 -0500

    dotenv

--- liri.js
+++ liri.js
@@ -3,10 +3,15 @@ var moment = require('moment');
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 const axios = require('axios');
-
+require('dotenv').config()
 var spotify = new Spotify(keys.spotify);
+const db = require('db')
+db.connect({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
 
-//need to install npm packages
 //Spotify: https://www.npmjs.com/package/node-spotify-api
 //Axios: https://www.npmjs.com/package/axios
 //Moment: https://momentjs.com/docs/
